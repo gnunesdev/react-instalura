3fbb61d46ae7f72a136ce38ce1a026f626d9dc09
gnunesinf <gnunesinf@gmail.com>
2019-05-21 07:37:33 -0300

    [React] solved redirect in module 4, finished :D

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -4,32 +4,42 @@ import { withRouter } from "react-router-dom";
 import FotoItem from "./Foto";
 
 class Timeline extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       fotos: []
     };
+    this.login = this.props.login;
   }
 
-  componentDidMount() {
+  carregaFotos(props) {
     let urlPerfil;
-
-    if (this.props.match.params.login === undefined) {
+    if (this.login === undefined) {
       urlPerfil = `https://instalura-api.herokuapp.com/api/fotos?X-AUTH-TOKEN=${localStorage.getItem(
         "auth-token"
       )}`;
     } else {
       urlPerfil = `https://instalura-api.herokuapp.com/api/public/fotos/${
-        this.props.match.params.login
+        this.login
       }`;
     }
 
     fetch(urlPerfil)
       .then(response => response.json())
       .then(fotos => {
-        console.log(fotos);
         this.setState({ fotos: fotos });
       });
+  }
+
+  componentDidMount() {
+    this.carregaFotos();
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.match.params.login !== undefined) {
+      this.login = nextProps.match.params.login;
+      this.carregaFotos(nextProps);
+    }
   }
 
   render() {
