568d967c42ee322d49866ee3dfd815477808e5db
gnunesinf <gnunesinf@gmail.com>
2019-05-22 07:15:28 -0300

    [React] Init module 5, create like function, stopped in refresh likers list after like a photo

--- src/components/Foto.js
+++ src/components/Foto.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import Pubsub from "pubsub-js";
 
 class FotoHeader extends Component {
   render() {
@@ -20,11 +21,24 @@ class FotoHeader extends Component {
 }
 
 class FotoInfo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { likers: this.props.foto.likers };
+  }
+
+  componentWillMount() {
+    Pubsub.subscribe("atualiza-liker", (topico, infoLiker) => {
+      const possivelLiker = this.state.likers.find(
+        liker => liker.login === infoLiker.liker.login
+      );
+    });
+  }
+
   render() {
     return (
       <div className="foto-info">
         <div className="foto-info-likes">
-          {this.props.foto.likers.map(liker => {
+          {this.state.likers.map(liker => {
             return (
               <Link key={liker.login} href={`/timeline/${liker.login}`}>
                 {liker.login},
@@ -58,10 +72,49 @@ class FotoInfo extends Component {
 }
 
 class FotoAtualizacoes extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      likeada: this.props.foto.likeada
+    };
+  }
+
+  like(ev) {
+    ev.preventDefault();
+    const requestDetails = {
+      method: "POST"
+    };
+
+    fetch(
+      `https://instalura-api.herokuapp.com/api/fotos/${
+        this.props.foto.id
+      }/like?X-AUTH-TOKEN=${localStorage.getItem("auth-token")}`,
+      requestDetails
+    )
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Não foi possível realizar o like da foto");
+        }
+      })
+      .then(liker => {
+        this.setState({ likeada: !this.state.likeada });
+        Pubsub.publish("atualiza-liker", { fotoId: this.props.foto.id, liker }); //property shorthand
+      });
+  }
+
   render() {
     return (
       <section className="fotoAtualizacoes">
-        <a href="#" className="fotoAtualizacoes-like">
+        <a
+          onClick={this.like.bind(this)}
+          className={
+            this.state.likeada
+              ? "fotoAtualizacoes-like-ativo"
+              : "fotoAtualizacoes-like"
+          }
+        >
           Likar
         </a>
         <form className="fotoAtualizacoes-form">
@@ -88,7 +141,7 @@ export default class FotoItem extends Component {
         <FotoHeader foto={this.props.foto} />
         <img alt="foto" className="foto-src" src={this.props.foto.urlFoto} />
         <FotoInfo foto={this.props.foto} />
-        <FotoAtualizacoes />
+        <FotoAtualizacoes foto={this.props.foto} />
       </div>
     );
   }
